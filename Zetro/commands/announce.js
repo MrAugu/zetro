687dad3c0366bf150d7c66eabf4f54579d63f5de
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-19 22:47:20 +0300

    Update announce.js
    Quick Update: Making available sending message in a custom channel

--- Zetro/commands/announce.js
+++ Zetro/commands/announce.js
@@ -3,10 +3,18 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
     if(!message.member.hasPermission(`MANAGE_MESSAGES`))
     return message.channel.send("You don't have permission to do that!");
+    
     if(!args[0]) 
     return message.channel.send("Please specify a text.");
-    let announcement = args.slice(0).join(" ");
+    
+    let channelname = args[0];
+    let channel = message.guild.channels.find("name", `${channelname}`);
+    if(!channelname)
+    return message.channel.send("Please name a valid channel Usage: `announce <channel name> <text>`");
+    
+    let announcement = args.slice(1).join(" ");
     message.delete();
+    
     const announcementEmbed = new Discord.RichEmbed()
     .setTitle("Announcement:")
     .setAuthor(message.author.username, message.author.avatarURL)
@@ -15,7 +23,6 @@ module.exports.run = async (client, message, args) => {
     .setFooter(`Announced by ${message.author.tag}`)
     .setTimestamp();
     message.channel.send(announcementEmbed);
-    return;
   }
   
   module.exports.help = {
