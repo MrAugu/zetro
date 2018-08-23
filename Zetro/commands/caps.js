af460cca872130092d5687d819f803e1cd705b20
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-23 22:14:50 +0300

    Making command "more understandable"

--- Zetro/commands/caps.js
+++ Zetro/commands/caps.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 const randomizeCase = word => word.split('').map(c => Math.random() > 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
 
 module.exports.run = async (client, message, args) => {
-  if (args.length < 1) return message.channel.send("**I need some text to clapify.** `ium clap <sentence>`")
+  if (!args[0]) return message.channel.send("Please specify a sentence to random capitalize.")
     message.channel.send(args.map(randomizeCase).join(':clap:'));
 }
 
 module.exports.help = {
-  name: "clap"
+  name: "caps"
 }
