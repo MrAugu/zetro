7b602b00715d46a6581cfeedd630fa5d99b87dc0
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-23 22:23:29 +0300

    What was wrong with me when i put these replys??

--- Zetro/commands/liedetect.js
+++ Zetro/commands/liedetect.js
@@ -4,10 +4,9 @@ module.exports.run = async (client, message, args) => {
     if(!args[0])
     return message.channel.send("Please specify a text.");
   const result = [
-      "False!", "True!"
+      "Thats completly false!", "Thats really true!"
   ]
   message.channel.send(`${result[Math.floor(Math.random() * result.length)]}`);
-  return;
 }
 
 module.exports.help = {
