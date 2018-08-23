4fe548135f287b28dd5a44b8edf87ccf6d79b457
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-23 23:56:19 +0300

    More Fail/Succes emojis there will be many more

--- Zetro/commands/announce.js
+++ Zetro/commands/announce.js
@@ -1,16 +1,18 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
+     let f = client.emojis.find("name", `zuncheck`);
+     let s = client.emojis.find("name", `zcheck`);
     if(!message.member.hasPermission(`MANAGE_MESSAGES`))
-    return message.channel.send("You don't have permission to do that!");
+    return message.channel.send(`${f} You don't have permission to do that!`);
     
     if(!args[0]) 
-    return message.channel.send("Please specify a text.");
+    return message.channel.send(`${f} Please specify a text.`);
     
     let channelname = args[0];
     let channel = message.guild.channels.find("name", `${channelname}`);
-    if(!channelname)
-    return message.channel.send("Please name a valid channel Usage: `announce <channel name> <text>`");
+    if(!channel)
+    return message.channel.send(`${f} Please name a valid channel of  this server!`);
     
     let announcement = args.slice(1).join(" ");
     message.delete();
@@ -18,11 +20,12 @@ module.exports.run = async (client, message, args) => {
     const announcementEmbed = new Discord.RichEmbed()
     .setTitle("Announcement:")
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setColor("#000000")
+    .setColor("RANDOM")
     .setDescription(`${announcement}`)
     .setFooter(`Announced by ${message.author.tag}`)
     .setTimestamp();
-    message.channel.send(announcementEmbed);
+    channel.send(announcementEmbed);
+    message.channel.send(`${s} Announcement has been sent!`);
   }
   
   module.exports.help = {
