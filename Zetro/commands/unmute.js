6508d9e7fa5d09460acaba82f242c6b9f22a97e8
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 16:20:52 +0300

    Update Unmute To New System and Fix Deprecations

--- Zetro/commands/unmute.js
+++ Zetro/commands/unmute.js
@@ -1,39 +1,27 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 
-module.exports.run = async (bot, message, args) => {
-
+exports.run = async (bot, message, args) => {
+  let f = client.emojis.find(c => c.name === "zuncheck");
+  let s = client.emojis.find(c => c.name === "zcheck");
 
   if(!message.member.hasPermission("MANAGE_MESSAGES"))
-  return message.channel.send("You don't have permission to do that!");
+  return message.channel.send(`${f} You don't have permission to do that!`);
+  
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!tomute)
-  return message.channel.send("Please mention a valid user.");
-  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Sorry, the user can't be unmuted.");
+  return message.channel.send(`${f} Please mention a valid user.`);
+  
+  if(tomute.hasPermission("MANAGE_MESSAGES"))
+   return message.channel.send(`${f} Sorry, the user can't be unmuted.`);
+  
   let reason = args.slice(1).join(" ");
   if(!reason)
-  return message.channel.send("Please specify a reason.");
-
-  let muterole = message.guild.roles.find(`name`, "Z-Muted");
+  return message.channel.send(`${f} Please specify a reason!`);
+  
+  let muterole = message.guild.roles.find(c => c.name === "Z-Muted");
     await(tomute.removeRole(muterole.id));
 
-    message.channel.send(`${tomute} has been unmuted succefully.`);
-    tomute.send(`You were unmuted in ${message.guild.name} because of ${reason}.`)
-    let logs = JSON.parse(fs.readFileSync("./logs.json", "utf8"));
-
-    if(!logs[message.guild.id]) {
-      logs[message.guild.id] = {
-        logs: "logs"
-      };
-    }
-
-    let log = logs[message.guild.id].logs;
-
-    let lc = message.guild.channels.find(`name`, log);
-   lc.send(`:loud_sound: **${message.author.tag}** unmuted **${tomute}**(ID:${tomute.id})\n**Reason**: ${reason}`);
-return;
-  }
-
-module.exports.help = {
-  name: "unmute"
-}
+    message.channel.send(`${s} **${tomute.user.tag}** has been unmuted succefully.`);
+  tomute.send(`You were unmuted in **${message.guild.name}** beacuse **${reason}**!`);
+  };
