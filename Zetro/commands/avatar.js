f376d8deeda8d7b45d5a851fe69521c19425da41
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 15:00:57 +0300

    Update Avatar To New System

--- Zetro/commands/avatar.js
+++ Zetro/commands/avatar.js
@@ -1,17 +1,13 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
+exports.run = async (client, message, args) => {
     let target = message.mentions.users.first() || message.author;
     message.channel.send({files: [
-{
-attachment: target.displayAvatarURL,
-name: "avatar.png"
+        {
+            attachment: target.displayAvatarURL,
+            name: "avatar.png"
 
-}
-]});
-    return;
-}
- 
-module.exports.help = {
-  name: "avatar"
-}
+        }
+    ]});
+  
+};
