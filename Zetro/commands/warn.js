0f0c6770bd182715496dfffc472db473136a66dd
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 16:25:46 +0300

    Update Warn To New System

--- Zetro/commands/warn.js
+++ Zetro/commands/warn.js
@@ -1,39 +1,24 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 
-module.exports.run = async (client, message, args) => {
+exports.run = async (client, message, args) => {
+    let f = client.emojis.find(c => c.name === "zuncheck");
+    let s = client.emojis.find(c => c.name === "zcheck");
+    
     if(!message.member.hasPermission("MANAGE_MESSAGES"))
-    return message.channel.send("You don't have permission to do that.");
+    return message.channel.send(`${f} You don't have permission to do that.`);
+    
     let member = message.mentions.members.first() || message.guild.members.get(args[0]);
     if(!member)
-    return message.channel.send("Please mention a valid user.");
+    return message.channel.send(`${f} Please mention a valid user.`);
+    
     let reason = args.slice(1).join(" ");
     if(!reason)
-    return message.channel.send("Please specify a reason.");
+    return message.channel.send(`${f} Please specify a reason.`);
+    
     if(member.hasPermission("MANAGE_GUILD")) 
-    return message.reply("This user can't be warned, sorry.");
-    message.channel.send(`**${member.user.tag}** has been warned for ${reason}!`);
-    try {
-        await member.send(`You were warned in **${message.guild.name}** for ${reason}.`);
-     } catch(err) {
-   
-     }
-     let logs = JSON.parse(fs.readFileSync("./logs.json", "utf8"));
-
-     if(!logs[message.guild.id]) {
-       logs[message.guild.id] = {
-         logs: "logs"
-       };
-     }
-
-     let log = logs[message.guild.id].logs;
-
-     let lc = message.guild.channels.find(`name`, log);
-      lc.send(`:triangular_flag_on_post: **${message.author.tag}** warned ${member.user.tag}(ID:${member.id})\n**Reason**: ${reason}`);
-return;
-
-
-}
-module.exports.help = {
-  name: "warn"
-}
+    return message.channel.send(`${f} This user can't be warned, sorry.`);
+    
+    message.channel.send(`${s} **${member.user.tag}** has been warned for ${reason}!`);
+    member.send(`You were warned in **${message.guild.name}** because of **${reason}**!`);
+};
