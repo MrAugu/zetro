83b6d8bbdb449c3994e3aff1ddd087b8a550c9a4
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 15:11:22 +0300

    Update Caps To New System And Add Emotes

--- Zetro/commands/caps.js
+++ Zetro/commands/caps.js
@@ -1,11 +1,9 @@
 const Discord = require("discord.js");
 const randomizeCase = word => word.split('').map(c => Math.random() > 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
 
-module.exports.run = async (client, message, args) => {
-  if (!args[0]) return message.channel.send("Please specify a sentence to random capitalize.")
+exports.run = async (client, message, args) => {
+  let f = client.emojis.find(c => c.name === "zuncheck");
+  if (!args[0]) 
+  return message.channel.send(`${f} Please specify a senence to random capitalise.`);
     message.channel.send(args.map(randomizeCase).join(':clap:'));
-}
-
-module.exports.help = {
-  name: "caps"
-}
+};
