afaaae4418d97749084085b2d2572dae66a518e2
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 15:28:03 +0300

    Update Help, Fix Bugs and Update to New System

--- Zetro/commands/help.js
+++ Zetro/commands/help.js
@@ -1,28 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
-    if(!args[0]) {
-        
-        user = message.channel;
-        
-        const help1Embed = new Discord.RichEmbed()
-         .setTitle("You can @mention me for prefix.")
-         .setDescription("__**Utility Commands**__:\n**poll** - Makes a poll that users can vote.\n**serverinfo** - Gives details about server.\n**avatar** - Gives you the avatar URL.\n**announce** - Embeds your announcement.\n**whois** - Tells details about a user.\n**servericon** - Displays a link to server icon.\n\n__**Fun Commands**__:\n**8ball** - Ask mystical 8ball.\n**catmeme** - Gives you a rndom cat meme.\n**dogmeme** - Gives you a random dog meme.\n**roll** - Rolls a dice.\n**flipcoin** - Flips a coin.\n**say** - Say message you sayd and delete some evidences.\n**illegal** - Declares thing you say illegal.\n**spin** - Spins a roulette wheel.\n**liedetect** - Activates the lie detector.\n**lovecalc** - Calculates the love between 2 things.\n\n__**General Commands**__:\n**support** - Gives you a link to join my server.\n**ping** - Checks the discord latency.\n**invite** - Gives you the link to invite me.\n**help** - Gives you a detailed list of commnads.\n")
-         .setFooter("Page 1/2")
-         .setTimestamp();
-
-
-         const help3Embed = new Discord.RichEmbed()
-         .setTitle("You can @mention me for prefix.")
-         .setDescription("**about** - Gives you details about bot.\n**uptime** - Gives you the time bot was up.\n**stats** - Gives you details about me.\n\n__**Moderation Commands**__:\n**mute** - Mutes a user.\n**lockdown** - Locks down a channel.\n**unlockdown** - Unlocks a channel from lockdown.\n**clear** - Clear a specifyed amout of messages.\n**unmute** - Unmutes a user.\n**kick** - Kicks a user from server.\n**ban** - Bans a user from server.\n**warn** - Warns a user.\n\n__**Economy Commands**__:\n**work** - Gives you some money.\n**bal** - Show your balance.\n**register** - Registering you to economy.\n**pay** - Pay a user.\n\n__**Setting Commands**__:\n**setprefix** - Sets a new prefix for server.\n**setlogs** - Sets log channel.\n**settings** - Shows current settings for server.\nSupport Server: https://discord.gg/QGJJBy6\nWebsite: **Comming Soon!**")
-         .setFooter("Page 2/2")
-         .setTimestamp();
-         
-         user.send(help1Embed);
-         user.send(help3Embed);
-    };
-    }
-
-module.exports.help = {
-  name: "help"
-}
+exports.run = async (client, message, args) => {
+    if(!args[0])
+    return message.channel.send("Syntax: `z!<command>`\n\n__**Utility Commands**__:\n**poll** - Creates a poll where users can vote.\n**serverinfo** - Gives details about the server.\n**avatar** - Gives you the avatar URL.\n**announce** - Embeds your announcement.\n**whois** - Tells details about a user.\n**servericon** - Displays a link to the server's icon.\n\n__**Fun Commands**__:\n**8ball** - Ask the mystical 8ball.\n**catmeme** - Gives you a random cat meme.\n**dogmeme** - Gives you a random dog meme.\n**roll** - Rolls a dice.\n**flipcoin** - Flips a coin.\n**say** - Repeats what you said and deletes your message.\n**illegal** - Declares the thing you said as illegal.\n**spin** - Spins a roulette wheel.\n**liedetect** - Activates the lie detector.\n**lovecalc** - Calculates the love between 2 things.\n\n__**General Commands**__:\n**support** - Gives you a link to join my server.\n**ping** - Checks the discord latency.\n**invite** - Gives you the link to invite me.\n**help** - Gives you a detailed list of commnads.\n**about** - Gives you details about bot.\n**uptime** - Echoes the bot's uptime.\n**stats** - Gives you details about me.\n\n__**Moderation Commands**__:\n**mute** - Mutes a user.\n**lockdown** - Locks down a channel.\n**unlockdown** - Unlocks a channel from lockdown.\n**clear** - Clears a specified amount of messages.\n**unmute** - Unmutes a user.\n**kick** - Kicks a user from the server.\n**ban** - Bans a user from the server.\n**warn** - Warns a user.\n\n__**Music Commands**__:\n**play** - Plays a song.\n**skip** - Skips the song currently playing.\n**stop** - Stops the currently playing music and clears the queue.\n**queue** - Shows the songs queue.\n**np** - Shows what's currently being played.\n**pause** - Pauses the current song.\n**resume** - Resumes the paused song.\n**volume** - Sets a custom volume.\n**mode** - Sets a custom mode.\n\n__**Setting Commands**__:\n**setprefix** - Sets a new prefix for the current server.\n**setlogs** - Sets the logs channel.\n**settings** - Shows the current settings for the server.\nSupport Server: https://discord.gg/QGJJBy6\nWebsite: **Comming Soon!**");
+};
