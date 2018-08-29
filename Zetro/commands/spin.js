0445db1126d5a8c6092a87c9ea843a394a112977
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 16:08:20 +0300

    Update It To The New System

--- Zetro/commands/spin.js
+++ Zetro/commands/spin.js
@@ -1,11 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
-    const spinmsg = await message.channel.send(`Spining wheel..`);
+exports.run = async (client, message, args) => {
+    const spinmsg = await message.channel.send(`Spining wheel...`);
     spinmsg.edit(`The wheel spins **${Math.floor(Math.random() * 36)}**!`);
-    return;
-}
-
-module.exports.help = {
-  name: "spin"
-}
+};
