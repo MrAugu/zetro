79f453b818a829a972d8a58607654f5018679d3e
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 15:53:32 +0300

    Update Mute To New System, Add Emotes and Fix Deprecations

--- Zetro/commands/mute.js
+++ Zetro/commands/mute.js
@@ -2,19 +2,24 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
-
+  let f = client.emojis.find(c => c.name === "zuncheck");
+  let s = client.emojis.find(c => c.name === "zcheck");
 
   if(!message.member.hasPermission("MANAGE_MESSAGES"))
-  return message.channel.send("You don't have permission to do that!");
+  return message.channel.send(`${f} You don't have permission to do that!`);
+  
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!tomute)
-  return message.channel.send("Please mention a valid user.");
-  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Sorry, the user can't be muted.");
+  return message.channel.send(`${f} Please mention a valid user!`);
+  
+  if(tomute.hasPermission("MANAGE_MESSAGES")) 
+  return message.channel.send(`${f} Sorry, the user can't be muted!`);
+  
   let reason = args.slice(1).join(" ");
   if(!reason)
-  return message.channel.send("Please specify a reason.");
+  return message.channel.send(`${f} Please specify a reason!`);
 
-  let muterole = message.guild.roles.find(`name`, "Z-Muted");
+  let muterole = message.guild.roles.find(r => r.name === "Z-Muted");
 
   if(!muterole){
     try{
@@ -36,24 +41,6 @@ module.exports.run = async (bot, message, args) => {
 
     await(tomute.addRole(muterole.id));
 
-    message.channel.send(`${tomute} has been muted succefully.`);
-    tomute.send(`You were muted in ${message.guild.name} because of ${reason}.`)
-    let logs = JSON.parse(fs.readFileSync("./logs.json", "utf8"));
-
-    if(!logs[message.guild.id]) {
-      logs[message.guild.id] = {
-        logs: "logs"
-      };
-    }
-
-    let log = logs[message.guild.id].logs;
-
-    let lc = message.guild.channels.find(`name`, log);
-
-   lc.send(`:mute: **${message.author.tag}** muted **${tomute}**(ID:${tomute.id})\n**Reason**: ${reason}`);
-return;
-  }
-
-module.exports.help = {
-  name: "mute"
-}
+    message.channel.send(`${s} **${tomute.user.tag}** has been muted succefully.`);
+    tomute.send(`You were muted in **${message.guild.name}** because of **${reason}**!`);
+};
