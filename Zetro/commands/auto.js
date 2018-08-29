5b7080719a4ec5554848cdf979028d78096ac2e3
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 14:59:21 +0300

    Update Auto To New System and Add Emotes

--- Zetro/commands/auto.js
+++ Zetro/commands/auto.js
@@ -2,8 +2,10 @@ const Discord = require("discord.js");
 const sm = require("string-similarity");
 const send = require("quick.hook");
 
-module.exports.run = async (client, message, args) => {
-  if(!args[0]) return message.reply("You need to type some text!");
+exports.run = async (client, message, args) => {
+  let f = client.emojis.find(c => c.name === "zuncheck");
+  
+  if(!args[0]) return message.reply(`${f} Please specify a text to match with.`);
    let members = [];
    let indexes = [];
    message.guild.members.forEach(function(member) {
@@ -15,8 +17,5 @@ module.exports.run = async (client, message, args) => {
    let member = message.guild.members.get(indexes[members.indexOf(username)]);
 
    message.channel.send(`The best match is **${member.user.username}**!`);
-}
-
-module.exports.help = {
-  name: "auto"
-}
+  
+};
