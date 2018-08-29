71e281505bf8ef48ebf66e3e8961a41dfb984a81
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 14:48:52 +0300

    Update 8ball.js

--- Zetro/commands/8ball.js
+++ Zetro/commands/8ball.js
@@ -1,15 +1,13 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
-     let f = client.emojis.find("name", `zuncheck`);
+exports.run = async (client, message, args) => {
+     let f = client.emojis.find(c => c.name === "zuncheck");
     if(!args[0])
     return message.channel.send(`${f} Please specify a text.`);
   const responses = [
-      " Yes.", "No.", "Maybe.", "Probably..", "Ask me later.."
+      " Yes!", "No!", "Maybe.", "Probably...", "Ask me later!"
   ]
+  
   message.channel.send(`${responses[Math.floor(Math.random() * responses.length)]}`);
-}
-
-module.exports.help = {
-  name: "8ball"
-}
+     
+};
