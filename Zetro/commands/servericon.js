6b40bcae530d9a255746cc484e4beca5905e5717
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 16:03:41 +0300

    Update Server Icon to new system

--- Zetro/commands/servericon.js
+++ Zetro/commands/servericon.js
@@ -1,11 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
+exports.run = async (client, message, args) => {
     let sicon = message.guild.iconURL;
     message.channel.send(`This server's icon is: ${sicon}`);
-    return;
-} 
-
-module.exports.help = {
-  name: "servericon"
-}
+};
