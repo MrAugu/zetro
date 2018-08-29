537c9f971bed4827fcc157cbe310b175a8940158
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 21:14:52 +0300

    GuildCreate event!

---
+++ Zetro/events/guildCreate.js
@@ -0,0 +1,17 @@
+exports.run = (client, guild) => {
+  const ServerLog = client.channels.get('484425064146665472');
+
+  let large = guild.large;
+  
+  if(large === true) large = "Yes";
+  if(large === false) large = "No"
+  
+  let joinEmbed = new Discord.RichEmbed()
+  .setTitle("Just Joined A Server!")
+  .setColor("#ff0000")
+  .setDescription(`Guild Name: **${guild.name}**\nMember Count: **${guild.memberCount}**\nLarge: **${large}**\nID: **${guild.id}** Users\nServer Count: **${client.guilds.size}**`)
+  .setTimestamp();
+  
+  ServerLog.send(joinEmbed);
+  
+ };
