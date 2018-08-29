efe09f40427f02d9d830df74d10a074c94a58067
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 15:08:22 +0300

    Update Ban Command

--- Zetro/commands/ban.js
+++ Zetro/commands/ban.js
@@ -1,41 +1,29 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 
-module.exports.run = async (client, message, args) => {
+exports.run = async (client, message, args) => {
+  let f = client.emojis.find(c => c.name === "zuncheck");
+  let s = client.emojis.find(c => c.name === "zcheck");
+  
   if(!message.member.hasPermission("BAN_MEMBERS"))
-  return message.channel.send("You don't have permission to do that!");
+  return message.channel.send(`${f} You do not have the permission to do so!`);
+  
   let member = message.mentions.members.first() || message.guild.members.get(args[0]);
   if(!member)
-  return message.channel.send("Please mention a valid user!");
+  return message.channel.send(`${f} Please mention a valid user!`);
+  
   let reason = args.slice(1).join(" ");
   if(!reason)
-  return message.channel.send("Please specify a reason.");
+  return message.channel.send(`${f} Please specify a reason!`);
+  
   if(message.member.id === member.id)
-  return message.channel.send("I cannot allow self-harm!");
+  return message.channel.send(`${f} Sorry but i cannot allow self-harm!`);
+  
   if(!member.bannable)
-  return message.channel.send("This member can't be banned!");
+  return message.channel.send(`${f} Oops, something went wrong while banning ${member.user.tag}!`);
+  
   await member.ban(reason)
-  message.channel.send(`**${member.user.tag}** has been banned succefully!`);
-  member.send(`You were banned from **${message.guild.name}** for ${reason}.`);
+  message.channel.send(`${s} **${member.user.tag}** has been banned succefully!`);
+  member.send(`You were banned from **${message.guild.name}** for **${reason}**!`);
 
-
-let logs = JSON.parse(fs.readFileSync("./logs.json", "utf8"));
-
-if(!logs[message.guild.id]) {
-  logs[message.guild.id] = {
-    logs: "logs"
-  };
-}
-
-let log = logs[message.guild.id].logs;
-
-let lc = message.guild.channels.find(`name`, log);
-
- lc.send(`:hammer: **${message.author.tag}** banned **${member.user.tag}**(ID:${member.id})\n**Reason**: ${reason}`);
-return;
-
-}
-
-module.exports.help = {
-  name: "ban"
-}
+};
