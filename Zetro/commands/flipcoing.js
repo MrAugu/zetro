7e4a6f18f5d83d2887c99a2b1676fb85003d37cc
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 15:22:58 +0300

    Update Flipcoing To The New System

--- Zetro/commands/flipcoing.js
+++ Zetro/commands/flipcoing.js
@@ -1,13 +1,8 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
-  const coinfaces = [
-  "Head!", "Tail!"
- ]
+exports.run = async (client, message, args) => {
+  
+  const coinfaces = ["Head!", "Tail!"]
  message.channel.send(`The face of coin signs ${coinfaces[Math.floor(Math.random() * coinfaces.length)]}`);
-return;
-}
- 
-module.exports.help = {
-  name: "flipcoin"
-}
+  
+};
