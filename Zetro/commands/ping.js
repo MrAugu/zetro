3a812731915ddf8afef7b898f29265126321ce14
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 15:54:42 +0300

    Update Ping To The New System

--- Zetro/commands/ping.js
+++ Zetro/commands/ping.js
@@ -1,11 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
+exports.run = async (client, message, args) => {
   const m = await message.channel.send("Checking Latency...");
   m.edit(`:ping_pong:\nDuration: **${m.createdTimestamp - message.createdTimestamp}**MS.\nWebsocket Latency: **${Math.round(client.ping)}** Miliseconds`).catch(O_o=>{});
- return;
-}
- 
-module.exports.help = {
-  name: "ping"
-}
+};
