492fcaa0979b8c5c6e2730ce4e05c464db18e458
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 15:44:21 +0300

    Update LieDetect To The New System

--- Zetro/commands/liedetect.js
+++ Zetro/commands/liedetect.js
@@ -1,14 +1,12 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
+exports.run = async (client, message, args) => {
+    let f = client.emojis.find(c => c.name === "zuncheck");
+    
     if(!args[0])
-    return message.channel.send("Please specify a text.");
-  const result = [
-      "Thats completly false!", "Thats really true!"
-  ]
+    return message.channel.send(`${f} Please specify a text.`);
+    
+  const result = ["Thats completly false!", "Thats completly true!"]
+  
   message.channel.send(`${result[Math.floor(Math.random() * result.length)]}`);
-}
-
-module.exports.help = {
-  name: "liedetect"
-}
+};
