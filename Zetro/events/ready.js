3ae920a0258364ed1f9963d56610aeb99aa1adb7
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 16:36:40 +0300

    Ready Event

--- Zetro/events/ready.js
+++ Zetro/events/ready.js
@@ -1,1 +1,13 @@
-//TEST TEST JUST TO BE SMTH
+const config = require("../config.json");
+
+exports.run = async (client) => {
+  let prefix = config.prefix;
+ setInterval(() => {
+	  let status = [`${client.guilds.size} servers | ${prefix}help`,`${client.users.size} users | ${prefix}help`];
+    let curStatus = status[Math.floor(Math.random() * status.length)];
+    client.user.setActivity(`${curStatus}`, { type: 'WATCHING' });
+ }, 5000);
+
+ await client.user.setStatus("online");
+ console.log(`Succefully Connected to ${client.guilds.size} servers, ${client.channels.size} channels and ${client.users.size} users!`);
+};
