cd65e1fe86979d792ce42e14f246580735932818
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-29 16:15:27 +0300

    Update It To New System And Add Emojis

--- Zetro/commands/unlockdown.js
+++ Zetro/commands/unlockdown.js
@@ -1,36 +1,24 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 
-module.exports.run = async (client, message, args) => {
+exports.run = async (client, message, args) => {
+    let f = client.emojis.find(c => c.name === "zuncheck");
+    let s = client.emojis.find(c => c.name === "zcheck");
+    
     if(!message.member.hasPermission("MANAGE_GUILD"))
-    return message.channel.send("You don't have permission to do that!");
+    return message.channel.send(`${f} You don't have permission to do that!`);
+    
     let reason = args.slice(0).join(" ");
     if(!reason)
-    return message.channel.send("Please specify a reason.");
+    return message.channel.send(`${f} Please specify a reason!`);
+    
     message.channel.overwritePermissions(message.guild.id, {
         SEND_MESSAGES: null,
         ADD_REACTIONS: null
       }).then(() => {
-      message.channel.send("Lockdown lifted!");
+      message.channel.send(`${s} Lockdown lifted, permissions are back to normal!`);
       }).catch(error => {
         console.log(error);
       });
-
-      let logs = JSON.parse(fs.readFileSync("./logs.json", "utf8"));
-
-      if(!logs[message.guild.id]) {
-        logs[message.guild.id] = {
-          logs: "logs"
-        };
-      }
-
-      let log = logs[message.guild.id].logs;
-
-      let lc = message.guild.channels.find(`name`, log);
-     lc.send(`:loud_sound: **${message.author.tag}** unlocked down <#${message.channel.id}>.\n**Channel**: <#${message.channel.id}>\n**Reason**: ${reason}`);
-return;
-}
-
-module.exports.help = {
-  name: "unlockdown"
-}
+    
+};
