30847a792a44f90151b0d96c40db93fe268c15f7
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-30 16:26:14 +0300

    Args Fix

--- Zetro/commands/liedetect.js
+++ Zetro/commands/liedetect.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
     let f = client.emojis.find(c => c.name === "zuncheck");
     
-    if(!args[0])
+    if(!args[1])
     return message.channel.send(`${f} Please specify a text.`);
     
   const result = ["Thats completly false!", "Thats completly true!"]
