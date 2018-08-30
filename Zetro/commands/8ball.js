ee432682d0af566d22800b98959ac447e0cd688a
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-08-30 15:54:52 +0300

    Argument Issue Fix

--- Zetro/commands/8ball.js
+++ Zetro/commands/8ball.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
      let f = client.emojis.find(c => c.name === "zuncheck");
-    if(!args[0])
+    if(!args[1])
     return message.channel.send(`${f} Please specify a text.`);
   const responses = [
       " Yes!", "No!", "Maybe.", "Probably...", "Ask me later!"
